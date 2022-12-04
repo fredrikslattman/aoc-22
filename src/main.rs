6034754088665a2c69706484bd77a3129fc61caf
Fredrik Slättman <fredrik.slattman@gmail.com>
2022-12-04 18:38:52 +0100

    solution for day 3

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+macro_rules! include_day3_input {
+    ($path:expr) => {
+        include_str!($path)
+            .lines()
+            .map(|line| line.chars().collect())
+            .collect()
+    };
+}
+
 macro_rules! include_day2_input {
     ($path:expr) => {
         include_str!($path)
@@ -32,6 +41,10 @@ fn main() {
     let input: Vec<(char, char)> = include_day2_input!("../input/2a.txt");
     println!("Day 2a: {}", day2_a(&input));
     println!("Day 2b: {}", day2_b(input));
+
+    let input: Vec<Vec<char>> = include_day3_input!("../input/3a.txt");
+    println!("Day 3a: {}", day3_a(&input));
+    println!("Day 3b: {}", day3_b(input));
 }
 
 pub fn day1_a(input: &Vec<Vec<u32>>) -> u32 {
@@ -82,6 +95,42 @@ pub fn day2_b(input: Vec<(char, char)>) -> u32 {
     })
 }
 
+pub fn day3_a(input: &Vec<Vec<char>>) -> u32 {
+    input
+        .clone()
+        .iter_mut()
+        .map(|first: &mut Vec<char>| {
+            let second = first.split_off(first.len() / 2);
+            let dupe = *first.iter().filter(|x| second.contains(x)).next().unwrap();
+            if dupe >= 'a' && dupe <= 'z' {
+                return (dupe as u32) - 96;
+            } else {
+                return (dupe as u32) - 38;
+            }
+        })
+        .sum()
+}
+
+pub fn day3_b(input: Vec<Vec<char>>) -> u32 {
+    input
+        .clone()
+        .windows(3)
+        .step_by(3)
+        .map(|window| {
+            let dupe = *window[0]
+                .iter()
+                .filter(|x| window[1].contains(x) && window[2].contains(x))
+                .next()
+                .unwrap();
+            if dupe >= 'a' && dupe <= 'z' {
+                return (dupe as u32) - 96;
+            } else {
+                return (dupe as u32) - 38;
+            }
+        })
+        .sum()
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -108,5 +157,17 @@ mod test {
     fn test_day_2b() {
         let input: Vec<(char, char)> = include_day2_input!("../input/2a_test.txt");
         assert_eq!(day2_b(input), 12);
+    }
+
+    #[test]
+    fn test_day_3a() {
+        let input: Vec<Vec<char>> = include_day3_input!("../input/3a_test.txt");
+        assert_eq!(day3_a(&input), 157);
+    }
+
+    #[test]
+    fn test_day_3b() {
+        let input: Vec<Vec<char>> = include_day3_input!("../input/3a_test.txt");
+        assert_eq!(day3_b(input), 70);
     }
 }
